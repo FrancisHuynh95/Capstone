e3535a7bae2d6d8044f7dc37c83ab67b99f222df
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-17 22:01:29 -0700

    update lol

--- react-app/src/components/updateProduct/index.js
+++ react-app/src/components/updateProduct/index.js
@@ -6,10 +6,11 @@ import { getSingleProductThunk } from "../../store/product"
 function UpdateProduct() {
     const currentProduct = useSelector(state => state.product.singleProduct)
     const currentProductArray = Object.values(currentProduct)
+    console.log(currentProductArray[0])
     const dispatch = useDispatch()
     const { productId } = useParams()
 
-    const [name, setName] = useState(currentProductArray[0]?.name)
+    const [name, setName] = useState("")
     const [price, setPrice] = useState(currentProductArray[0]?.price)
     const [description, setDescription] = useState(currentProductArray[0]?.description)
     const [product_img1, setImg1] = useState(null)
@@ -21,96 +22,114 @@ function UpdateProduct() {
 
     useEffect(() => {
         dispatch(getSingleProductThunk(productId))
-    }, [dispatch])
+    }, [dispatch, productId])
 
     useEffect(() => {
-        if(currentProductArray){
+        if (currentProductArray.length > 0) {
             setName(currentProductArray[0].name)
             setPrice(currentProductArray[0].price)
             setDescription(currentProductArray[0].description)
         }
-    }, [productId, name, price, description])
+    }, [productId, currentProduct])
 
-    function handleUpdateButton(){
+    function handleUpdateButton() {
         console.log('weeeeenis')
     }
 
 
-    if (!currentProduct) return null
+    if (!Object.values(currentProduct).length) return null
     return (
         <>
-            <h1>Update Product</h1>
-            <div>
-                <label>
-                    <div>Name</div>
-                    <input type="text" value={name} placeholder="Name"></input></label>
-            </div>
-            <div>
-                <label>
-                    <div>price</div>
-                    <input type="text" value={price} placeholder="Price"></input></label>
-            </div>
-            <div>
-                <label>
-                    <div>Description</div>
-                    <textarea value={description} placeholder="Description"></textarea>
-                </label>
-            </div>
-            <div>
-            <label>
-                    <div>First Product Image</div>
-                    <input
-                        className="product_img"
-                        type='file'
-                        name="product_img1"
-                        accept="image/*"
-                        onChange={(e) => setImg1(e.target.files[0])}
-                    ></input>
-                </label>
-            <label>
-                    <div>Second Product Image</div>
-                    <input
-                        className="product_img"
-                        type='file'
-                        name="product_img1"
-                        accept="image/*"
-                        onChange={(e) => setImg2(e.target.files[0])}
-                    ></input>
-                </label>
-            <label>
-                    <div>Three Product Image</div>
-                    <input
-                        className="product_img"
-                        type='file'
-                        name="product_img1"
-                        accept="image/*"
-                        onChange={(e) => setImg3(e.target.files[0])}
-                    ></input>
-                </label>
-            <label>
-                    <div>Four Product Image</div>
-                    <input
-                        className="product_img"
-                        type='file'
-                        name="product_img1"
-                        accept="image/*"
-                        onChange={(e) => setImg4(e.target.files[0])}
-                    ></input>
-                </label>
-            <label>
-                    <div>Five Product Image</div>
-                    <input
-                        className="product_img"
-                        type='file'
-                        name="product_img1"
-                        accept="image/*"
-                        onChange={(e) => setImg5(e.target.files[0])}
-                    ></input>
-                </label>
-            </div>
-            <div>
-            <button onClick={() => handleUpdateButton()}>Update Product</button>
-            </div>
+            <form method="PUT">
+                <h1>Update Product</h1>
+                <div>
+                    <label>
+                        <div>Name</div>
+                        <input
+                            type="text"
+                            value={name || ""}
+                            placeholder="Name"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <div>price</div>
+                        <input
+                        type="text"
+                        value={price || 0}
+                        placeholder="Price"
+                        onChange={(e) => setPrice(e.target.value)}
+                        />
+                        </label>
+                </div>
+                <div>
+                    <label>
+                        <div>Description</div>
+                        <textarea
+                        value={description}
+                        placeholder="Description"
+                        onChange={e => setDescription(e.target.value)}
+                        ></textarea>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <div>First Product Image</div>
+                        <input
+                            className="product_img"
+                            type='file'
+                            name="product_img1"
+                            accept="image/*"
+                            onChange={(e) => setImg1(e.target.files[0])}
+                        ></input>
+                    </label>
+                    <label>
+                        <div>Second Product Image</div>
+                        <input
+                            className="product_img"
+                            type='file'
+                            name="product_img1"
+                            accept="image/*"
+                            onChange={(e) => setImg2(e.target.files[0])}
+                        ></input>
+                    </label>
+                    <label>
+                        <div>Three Product Image</div>
+                        <input
+                            className="product_img"
+                            type='file'
+                            name="product_img1"
+                            accept="image/*"
+                            onChange={(e) => setImg3(e.target.files[0])}
+                        ></input>
+                    </label>
+                    <label>
+                        <div>Four Product Image</div>
+                        <input
+                            className="product_img"
+                            type='file'
+                            name="product_img1"
+                            accept="image/*"
+                            onChange={(e) => setImg4(e.target.files[0])}
+                        ></input>
+                    </label>
+                    <label>
+                        <div>Five Product Image</div>
+                        <input
+                            className="product_img"
+                            type='file'
+                            name="product_img1"
+                            accept="image/*"
+                            onChange={(e) => setImg5(e.target.files[0])}
+                        ></input>
+                    </label>
+                </div>
+                <div>
+                    <button onClick={() => handleUpdateButton()}>Update Product</button>
+                </div>
+            </form>
         </>
     )
 }
