27d64b3de0e18d13f70314a14f5332903b1edbbe
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-18 21:10:41 -0700

    27 seeded products.. yay

--- app/seeds/product_seed.py
+++ app/seeds/product_seed.py
@@ -112,8 +112,163 @@ def seed_products(seeded_users):
         product_img5 = 'http://keebsy.s3.amazonaws.com/24dad646cdf444428f6635ed9668d602.jpg',
         user = seeded_users[1]
     )
+    product11 = Product(
+        name = 'Keychron Q10 75 Kit',
+        price = 195.00,
+        description = "The Keychron Q10 is a 75% Alice aluminum keyboard that is hot-swappable and works with VIA/QMK.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/ca81ed4aa5744085beffa44981b1875f.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/6f5494201a174af5b47f746fc13f6d86.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/5a908a0563d54028a156c8cda83abc14.png',
+        product_img4 = 'http://keebsy.s3.amazonaws.com/9281983601af4fdeae3117f34f36f1a6.png',
+        product_img5 = 'http://keebsy.s3.amazonaws.com/f643087022c94e8fad2489bfcf9aa2e6.png',
+        user = seeded_users[2]
+    )
+    product12 = Product(
+        name = 'White Coiled Cable',
+        price = 49.99,
+        description = "Elevate your keyboard setup to the next level with the CableMod Pro Keyboard Cable. Made for keyboards with a USB-C port, this hand-made keyboard cable is sleeved with both ModFlex and ModMesh sleeving, and is the ultimate accessory to make your keyboard setup pop.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/b5ae07e24e984610991a3abc54889e5a.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/478ce31803cd481f9cbd332bc0f3ef75.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/8db0c20e279b4c4090a5d6c6668e2905.jpg',
+        user = seeded_users[2]
+    )
+    product13 = Product(
+        name = 'Black Coiled Cable',
+        price = 49.99,
+        description = "Elevate your keyboard setup to the next level with the CableMod Pro Keyboard Cable. Made for keyboards with a USB-C port, this hand-made keyboard cable is sleeved with both ModFlex and ModMesh sleeving, and is the ultimate accessory to make your keyboard setup pop.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/850287cb7ee5486890c946a3a5a0fb5c.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/11d186b04dcd4f13a3db1874205414b9.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/e11418280cf54a2b8ae0006c1c21ba39.jpg',
+        user = seeded_users[2]
+    )
+    product14 = Product(
+        name = 'Red Coiled Cable',
+        price = 49.99,
+        description = "Elevate your keyboard setup to the next level with the CableMod Pro Keyboard Cable. Made for keyboards with a USB-C port, this hand-made keyboard cable is sleeved with both ModFlex and ModMesh sleeving, and is the ultimate accessory to make your keyboard setup pop.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/7b2f93ae5c6c48bda15885c64703b793.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/77e5c70763f945178f20d51ce82c063b.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/d6911ab0b015429cb09ac5f1cad54d65.jpg',
+        user = seeded_users[2]
+    )
+    product15 = Product(
+        name = 'Grey Coiled Cable',
+        price = 49.99,
+        description = "Elevate your keyboard setup to the next level with the CableMod Pro Keyboard Cable. Made for keyboards with a USB-C port, this hand-made keyboard cable is sleeved with both ModFlex and ModMesh sleeving, and is the ultimate accessory to make your keyboard setup pop.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/a56d610010b944a8801774de5fd4acda.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/3ee9821af31e4ee19763e180fc363434.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/fa4c4287a2954ce584da2514e832ca0d.jpg',
+        user = seeded_users[2]
+    )
+    product16 = Product(
+        name = 'Rainbow Coiled Cable',
+        price = 49.99,
+        description = "Elevate your keyboard setup to the next level with the CableMod Pro Keyboard Cable. Made for keyboards with a USB-C port, this hand-made keyboard cable is sleeved with both ModFlex and ModMesh sleeving, and is the ultimate accessory to make your keyboard setup pop.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/0c5ed8b082b045fe8cd2da0634bdabba.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/6cabaadd61c64ad7b5093888ba8cfc88.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/4879d0bf285f404c93605b51e3776f91.jpg',
+        user = seeded_users[2]
+    )
+    product17 = Product(
+        name = 'Spring Time Desk Mat',
+        price = 23.99,
+        description = "Made in collaboration with Hipyo Tech, the Spring Time mat brings life and color to any setup. Art by Eager Sprout.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/fa6955645bf04641a4610c944276dfb6.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/c9a31ad69dc84f60b470a2c7cfc26b92.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/199e698d6bb24f718298964e39172225.png',
+        user = seeded_users[1]
+    )
+    product18 = Product(
+        name = 'Yosemite Desk Mat',
+        price = 23.99,
+        description = "An abstract design on the mountains surrounding Yosemite, this vibrant desk mat looks great with all setups. Dimensions: 900x400x4mm",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/0d95c8d3dd354db487cf11a9ebddff49.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/a980d5291e414c44a6f12cfc4fff0a9d.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/9e539f306ce44ab18a4343e527889072.png',
+        user = seeded_users[1]
+    )
+    product19 = Product(
+        name = 'Coffee Shop Desk Mat',
+        price = 23.99,
+        description = "Up your productivity with a sip of inspiration—the Coffee Shop desk mat by Hipyo Tech! 900x400x4mm.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/24a06816b0a848848e4ef205ec627881.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/25455de2d7734f45a6fabcaec1e607e1.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/f03a3b9d92504bc1aedeed296c1edf41.png',
+        user = seeded_users[1]
+    )
+    product20 = Product(
+        name = 'Sleeping Kitty Desk Mat',
+        price = 23.99,
+        description = "Napping and kittens, what's better than that? Add this colorful desk mat to complete your purr-fect setup! Dimensions: 900 x 400 x 4mm.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/cc015616e1a74aed84caaa2fb03054ca.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/a7e13566ab7b43c0a8c0054779b136d6.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/7735d9033d0b45e387b6b7e6d216a0f7.png',
+        user = seeded_users[1]
+    )
+    product21 = Product(
+        name = 'Shibe Desk Mat',
+        price = 23.99,
+        description = "Designed by the multi-talented Apiary Keyboards, this desk mat features an adorable shiba that will make your setup stand out. Dimensions: 900 x 400 x 4mm.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/69afccef95494d6e8a9fcf262413b09d.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/c54d5e5db49541b7ab3a2f244a6b05dc.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/15698032561444338b1dc704a5ce0b7a.png',
+        user = seeded_users[0]
+    )
+    product22 = Product(
+        name = 'Seal Desk Mat',
+        price = 23.99,
+        description = "Designed by the multi-talented Apiary Keyboards, this desk mat features an adorable seal that will make your setup stand out. Dimensions: 900 x 400 x 4mm.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/a83f855c0887413a8c47ddc2b01b1f19.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/ffa3e18024f042bfbfcf88ffeba13d4c.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/3dc475ee158e4d4f9d9561fad6dff2f0.png',
+        user = seeded_users[0]
+    )
+    product23 = Product(
+        name = 'Pixel Comet Desk Mat',
+        price = 23.99,
+        description = "Made in collaboration with Hipyo Tech and pixel artist Eager Sprout, the Pixel Comet desk mat features a pixelated purple sky that will make your set up stand out! Dimensions: 900 x 400 x 4mm.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/a30c5e002ff74b339a79396d94cb2594.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/1d2fc32744e046fa9b6cdebc9f41a15d.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/60825b1f328c401aa08f1b2bd77cfefb.png',
+        user = seeded_users[0]
+    )
+    product24 = Product(
+        name = 'Sunny Day Desk Mat',
+        price = 23.99,
+        description = "Large desk mat, featuring a Sunny Day theme with pink and blue hues. The perfect combination for your RGB desk set up! Designed by Addmeonebay. Dimensions: 900 x 400 x 4mm.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/b785272dc2704888b32478c2a7cc0e58.png',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/256d46e60ccb4d43994ace6f5a3f2c4e.png',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/15403a10c0b14071b1a90c866040da21.png',
+        user = seeded_users[0]
+    )
+    product25 = Product(
+        name = 'Interstellar Artisan Keycap',
+        price = 40.99,
+        description = "Interstellar Artisan KeycapIs there anything more beautiful and awe-inspiring than the great cosmos? Despite the advancements in space science, what lies beyond the confines of our planet continues to be a mystery. It’s that constant search for answers that keep space enthusiasts hooked.",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/a359248b93554ad1a1f4cdec613bab09.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/93da94b21e244b76be94f0f75b06b445.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/c673ae28bd16494b992d33af9ec7bc9f.jpg',
+        user = seeded_users[1]
+    )
+    product26 = Product(
+        name = 'Fuji Mountain Artistan keycap',
+        price = 20.99,
+        description = "Fuji Mountain keycap for cherry MX keyboard, Custom keycap SA profile keycap for gaming keyboard, Custom mechanical keyboard,Christmas Gifts",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/ea90f4598cb741839b169bd63cc7df73.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/ea5de5004ce14cf08fd2fd598c5f5ad9.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/602b9000820b46d48e5bb90e5aa21149.jpg',
+        user = seeded_users[2]
+    )
+    product27 = Product(
+        name = 'White Cat Artistan Keycap',
+        price = 9.99,
+        description = "White Cat Keycaps Keycap set Esc Keycap Handmade SA and OEM Keycaps Custom Kitty Resin Artisan Keycap For Cherry MX Mechanical Keyboard cap",
+        product_img1 = 'http://keebsy.s3.amazonaws.com/28e0f9634c1245eeac0c766d6ad19d56.jpg',
+        product_img2 = 'http://keebsy.s3.amazonaws.com/f4b1b2c524b34f5d82371b1d70e2e327.jpg',
+        product_img3 = 'http://keebsy.s3.amazonaws.com/ec909bdfb45a48c2871453a4e57e71d4.jpg',
+        user = seeded_users[0]
+    )
 
-    all_products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10]
+    all_products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, product13, product14, product15, product16, product17, product18, product19, product20, product21, product22, product23, product24, product25, product26, product27]
     add_products = [db.session.add(product) for product in all_products]
     db.session.commit()
 
