7cf8c943c279f1963b81f11fb7aea9014db2987c
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-23 16:35:02 -0700

    linked the update button on user page

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -1,16 +1,18 @@
 import { useEffect } from "react"
-import { useSelector, useDispatch  } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { getAllProductsThunk } from "../../store/product"
 import "./userprofile.css"
+import { NavLink } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 
 const UserProfile = () => {
     const user = useSelector(state => state.session.user)
+    const history = useHistory()
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getAllProductsThunk())
     }, [])
-
 
     return (
         <>
@@ -19,16 +21,18 @@ const UserProfile = () => {
             <div className="userProductsEverything">
                 {user.product.map(product =>
                     <div className="userProductsContainer">
-                        <p className="user_product_name">{product.name}</p>
-                        <div className="UserProductPic">
-                        <img className="UserProductImg" src={`${product.product_img1}`}></img>
-                        </div>
+                        <NavLink exact to={`/products/${product.id}`}>
+                            <p className="user_product_name">{product.name}</p>
+                            <div className="UserProductPic">
+                                <img className="UserProductImg" src={`${product.product_img1}`}></img>
+                            </div>
+                        </NavLink>
                         <div className="userButtons">
-                        <button>Update</button>
-                        <button>Delete</button>
+                            <button onClick={() => history.push(`/products/${product.id}/update`)}>Update</button>
+                            <button>Delete</button>
                         </div>
                     </div>
-                    )}
+                )}
             </div>
             <div className="userReviews">
                 <h2>My Reviews</h2>
@@ -36,15 +40,15 @@ const UserProfile = () => {
                     <div className="userReviewsContainer">
                         <p className="user_product_name_review">{product.product_id}</p>
                         <div className="UserReview">
-                        <p className="user_product_name_review">{product.review}</p>
-                        <p className="user_product_name_review">{product.star_rating}</p>
+                            <p className="user_product_name_review">{product.review}</p>
+                            <p className="user_product_name_review">{product.star_rating}</p>
                         </div>
                         <div className="userButtons">
-                        <button>Update</button>
-                        <button>Delete</button>
+                            <button>Update</button>
+                            <button>Delete</button>
                         </div>
                     </div>
-                    )}
+                )}
             </div>
         </>
     )
