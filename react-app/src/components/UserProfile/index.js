d65b36d520ad6edd8972c94179f8f84dfe436899
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-25 11:42:38 -0700

    gave modal buttons an id

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -12,7 +12,6 @@ const UserProfile = () => {
     const user = useSelector(state => state.session.user)
     const allProducts = useSelector(state => state.product.products)
     const allProductArray1 = Object.values(allProducts)
-    console.log('allProductArray1 ==========>', allProductArray1)
     const reviewArray = []
     const theProduct = []
     for (let i = 0; i < allProductArray1.length; i++) {
@@ -23,8 +22,6 @@ const UserProfile = () => {
             }
         }
     }
-    console.log('review array ======================>', reviewArray)
-    console.log('review array ======================>', theProduct)
     const filiteredProducts = allProductArray1.filter(product => product.user.id === user.id)
     const history = useHistory()
     const dispatch = useDispatch()
@@ -43,15 +40,16 @@ const UserProfile = () => {
             <div className="userProductsEverything">
                 {filiteredProducts.map(product =>
                     <div className="userProductsContainer">
-                        <NavLink exact to={`/products/${product.id}`}>
+                        <NavLink className="userProfileProduct" exact to={`/products/${product.id}`}>
                             <p className="user_product_name">{product.name}</p>
                             <div className="UserProductPic">
                                 <img className="UserProductImg" src={`${product.product_img1}`}></img>
                             </div>
                         </NavLink>
                         <div className="userButtons">
-                            <button onClick={() => history.push(`/products/${product.id}/update`)}>Update</button>
+                            <button id="updateProductUserProfile" onClick={() => history.push(`/products/${product.id}/update`)}>Update</button>
                             <OpenModalButton
+                                id="deleteProductUserProfile"
                                 buttonText={`Delete Product`}
                                 modalComponent={<DeleteProductModal
                                     product_id={product.id}
@@ -74,7 +72,7 @@ const UserProfile = () => {
                             <p className="user_product_name_review">{theProduct[i].name}</p>
                             <p className="user_product_name_review">{product.review}</p>
                         </div>
-                        <div className="userButtons">
+                        <div className="userButtonsReview">
                             <OpenModalButton
                                 buttonText={`Update Review`}
                                 modalComponent={<UpdateReviewModal2
