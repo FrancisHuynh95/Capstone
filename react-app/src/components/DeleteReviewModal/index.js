6d2bf5d6c0475011d201145ed1179913e08b8326
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-25 16:19:00 -0700

    styled the delete review modal

--- react-app/src/components/DeleteReviewModal/index.js
+++ react-app/src/components/DeleteReviewModal/index.js
@@ -1,21 +1,24 @@
 import { useModal } from "../../context/Modal"
 import { useDispatch } from "react-redux"
 import { deleteReviewThunk } from "../../store/product"
+import './deleteReview.css'
 
-const DeleteReviewModal = ({review_id, product_id}) => {
+const DeleteReviewModal = ({ review_id, product_id }) => {
     const dispatch = useDispatch()
-    const {closeModal} = useModal()
+    const { closeModal } = useModal()
 
     const handleYes = async () => {
         await dispatch(deleteReviewThunk(review_id, product_id))
         closeModal()
     }
-    return(
-        <>
-        <h1>Are you sure you want to delete your review?</h1>
-        <button onClick={() => handleYes()}>Confirm Delete</button>
-        <button onClick={() => closeModal()}>Cancel</button>
-        </>
+    return (
+        <div className="deleteReviewModal">
+            <h2 className="deleteReviewModalH2">Are you sure you want to delete your review?</h2>
+            <div className="deleteReviewButtons">
+                <button className="deleteReviewButton" onClick={() => handleYes()}>Confirm Delete</button>
+                <button className="cancelButton" onClick={() => closeModal()}>Cancel</button>
+            </div>
+        </div>
     )
 }
 
