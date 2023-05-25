3c0db5060f908c95f0f01f921a059e206fb85d8a
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-25 16:14:04 -0700

    styled the delete product modal

--- react-app/src/components/deleteProductModal/index.js
+++ react-app/src/components/deleteProductModal/index.js
@@ -2,26 +2,29 @@ import { useModal } from "../../context/Modal"
 import { useDispatch } from "react-redux"
 import { useHistory } from "react-router-dom"
 import { deleteProductThunk } from "../../store/product"
+import "./deleteProduct.css"
 
 
-function DeleteProductModal({product_id}) {
+function DeleteProductModal({ product_id }) {
     const { closeModal } = useModal()
     const history = useHistory()
     const dispatch = useDispatch()
 
 
-    console.log('INSIDE DELETE PRODUCT MODAL',product_id)
-    async function handleOnClick(){
+    console.log('INSIDE DELETE PRODUCT MODAL', product_id)
+    async function handleOnClick() {
         await dispatch(deleteProductThunk(product_id))
         closeModal()
         history.push('/')
     }
     return (
-        <>
-            <h2>Delete Product Modal</h2>
-            <button onClick={() => handleOnClick()}>Confirm Delete</button>
-            <button onClick={() =>closeModal()}>Cancel</button>
-        </>
+        <div className="deleteProductModal">
+            <h2 className="deleteProductModalH2">Are you sure you want to delete the product listing?</h2>
+            <div className="deleteProductButtons">
+                <button className="deleteProductButton" onClick={() => handleOnClick()}>Confirm Delete</button>
+                <button className="cancelButton" onClick={() => closeModal()}>Cancel</button>
+            </div>
+        </div>
     )
 }
 
