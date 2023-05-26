762471f23487306e460b3af70b20eb8e48c6cce1
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-25 22:32:16 -0700

    update your review modal v2 css complete

--- react-app/src/components/updateReviewModal2/index.js
+++ react-app/src/components/updateReviewModal2/index.js
@@ -6,21 +6,21 @@ import { useModal } from "../../context/Modal"
 import { useSelector } from "react-redux"
 
 
-const UpdateReviewModal2 = ({product_id, review_id}) => {
+const UpdateReviewModal2 = ({ product_id, review_id }) => {
     const user = useSelector(state => state.session.user)
     const allProducts = useSelector(state => state.product.products)
     const singleProduct = useSelector(state => state.product.singleProduct)
     const allProductArray1 = Object.values(allProducts)
 
     let reviewArray
-    for(let i = 0; i < allProductArray1.length; i++){
-        for(let j = 0; j < allProductArray1[i].reviews.length; j++){
-            if(allProductArray1[i].reviews[j].id === review_id){
+    for (let i = 0; i < allProductArray1.length; i++) {
+        for (let j = 0; j < allProductArray1[i].reviews.length; j++) {
+            if (allProductArray1[i].reviews[j].id === review_id) {
                 reviewArray = (allProductArray1[i].reviews[j])
             }
         }
     }
-    const {closeModal} = useModal()
+    const { closeModal } = useModal()
     const dispatch = useDispatch()
     const [review, setReview] = useState("")
     const [stars, setStars] = useState(0)
@@ -28,8 +28,8 @@ const UpdateReviewModal2 = ({product_id, review_id}) => {
 
 
     useEffect(() => {
-        if(reviewArray?.review) setReview(reviewArray?.review)
-        if(reviewArray?.star_rating) setStars(reviewArray?.star_rating)
+        if (reviewArray?.review) setReview(reviewArray?.review)
+        if (reviewArray?.star_rating) setStars(reviewArray?.star_rating)
 
     }, [allProducts, dispatch])
 
@@ -51,26 +51,28 @@ const UpdateReviewModal2 = ({product_id, review_id}) => {
     }
     return (
 
-        <>
+        <div className="updateReviewModal">
             <form onSubmit={handleSubmit} method="PUT">
-                <h1>Update Review Modal</h1>
+                <h1 className="updateReviewModalH1">Update Your Review </h1>
                 {review.length < 10 && <p className="errors">Review must be at least 10 characters!</p>}
 
                 <div className="review_area">
                     <textarea rows={8} cols={45} placeholder="Write your review here!" value={review} onChange={e => setReview(e.target.value)}></textarea>
-                    <div className="Stars">
-                        <StarRating
-                            disabled={false}
-                            onChange={onChange}
-                            rating={stars}
-                        />
+                    <div className="updateReviewStars">
+                        <div className="Stars">
+                            <StarRating
+                                disabled={false}
+                                onChange={onChange}
+                                rating={stars}
+                            />
+                        </div>
+                    </div>
+                    <div id="buttonDiv">
+                        <button id="submitReviewButton" disabled={review.length < 10 || stars === 0 ? true : false} type="submit">Submit Your Review</button>
                     </div>
                 </div>
-                <div id="buttonDiv">
-                    <button id="submitReviewButton" disabled={review.length < 10 || stars === 0 ? true : false} type="submit">Submit Your Review</button>
-                </div>
             </form>
-        </>
+        </div>
     )
 }
 
