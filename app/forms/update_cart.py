9e589647a21d336c208331f5761cf0970bb6affe
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-06-13 14:21:36 -0700

    created update cart form

--- app/forms/update_cart.py
+++ app/forms/update_cart.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
 from wtforms.validators import DataRequired
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from app.api.aws_helpers import ALLOWED_IMAGES
+
 
 class UpdateProductForm(FlaskForm):
     quantitiy = IntegerField('quantity', validators=[DataRequired()])
