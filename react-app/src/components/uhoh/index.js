4bb749905825762b19f9379a4dfaec9334944ad5
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-06-13 23:11:27 -0700

    added pics

--- react-app/src/components/uhoh/index.js
+++ react-app/src/components/uhoh/index.js
@@ -1,8 +1,16 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+
 function Uhoh() {
+    const history = useHistory()
+    function handleRedirect() {
+        history.push('/')
+    }
     return (
         <>
-            <p>Uh Oh</p>
-            <img src="./cat404.jpg"></img>
+            <div className="uhohPage">
+                <img src="./cat404.jpg"></img>
+            </div>
+                <button onClick={() => handleRedirect()}>Take me back!</button>
         </>
     )
 }
