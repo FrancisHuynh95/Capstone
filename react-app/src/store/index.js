f7d382c09dd35f24b55a0e1a6f2b4a3a826c0cfa
FrancisHuynh95 <huynh_francis95@icloud.com>
2024-02-12 17:38:39 -0800

    removed reviews files from store

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import session from './session'
 import productReducer from './product';
 import cartReducer from './cart';
-// import reviewReducer from './review';
 
 const rootReducer = combineReducers({
   session,
