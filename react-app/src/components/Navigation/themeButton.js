96f5ee4223edc96b4be4dc44d5668b5013778ae1
FrancisHuynh95 <huynh_francis95@icloud.com>
2024-04-19 12:32:04 -0700

    added package for dark mode switch

--- react-app/src/components/Navigation/themeButton.js
+++ react-app/src/components/Navigation/themeButton.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { useTheme } from "./darkMode"
+import { DarkModeSwitch } from 'react-toggle-dark-mode'
 
 function ThemeButton(){
     const { toggleTheme } = useTheme()
     return(
-        <button onClick={toggleTheme}>Test</button>
+        // <button onClick={toggleTheme}>Test</button>
+        <DarkModeSwitch
+        checked
+        >
+        </DarkModeSwitch>
     )
 }
 
