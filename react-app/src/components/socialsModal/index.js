33bfbd8b3c4f6206c4356928da2b0e683ee841e9
FrancisHuynh95 <huynh_francis95@icloud.com>
2024-04-19 13:29:59 -0700

    experimenting with readability

--- react-app/src/components/socialsModal/index.js
+++ react-app/src/components/socialsModal/index.js
@@ -2,7 +2,7 @@ import './socials.css'
 import { useTheme } from '../Navigation/darkMode'
 
 const SocialsModal = () => {
-    const {theme} = useTheme()
+    const { theme } = useTheme()
     console.log(theme)
 
     const stylesLight = {
@@ -16,24 +16,55 @@ const SocialsModal = () => {
         backgroundColor: "black"
     }
     return (
-        <div className="socials" style = {theme === 'light' ? null: backgroundDark}>
-            <img className='software_engineer' src={theme === 'light' ? "/SoftwareEngineer.png" : "/SoftwareEngineerWhite.png"} alt='logo'></img>
-            <div className='whatItsMadeOutOf'>
-                <h3 style={theme === "light" ? stylesLight : stylesDark}>This website was made with:</h3>
-                <p style={theme === "light" ? stylesLight : stylesDark}>JavaScript</p>
-                <p style={theme === "light" ? stylesLight : stylesDark}>React.js/Redux</p>
-                <p style={theme === "light" ? stylesLight : stylesDark}>Python</p>
-                <p style={theme === "light" ? stylesLight : stylesDark}>Flask</p>
-
+        <div
+            className="socials"
+            style={theme === 'light' ? null : backgroundDark}>
+            <img
+                className='software_engineer'
+                src={theme === 'light' ? "/SoftwareEngineer.png" : "/SoftwareEngineerWhite.png"}
+                alt='logo'></img>
+            <div
+                className='whatItsMadeOutOf'>
+                <h3
+                    style={theme === "light" ? stylesLight : stylesDark}>This website was made with:</h3>
+                <p
+                    style={theme === "light" ? stylesLight : stylesDark}>JavaScript</p>
+                <p
+                    style={theme === "light" ? stylesLight : stylesDark}>React.js/Redux</p>
+                <p
+                    style={theme === "light" ? stylesLight : stylesDark}>Python</p>
+                <p
+                    style={theme === "light" ? stylesLight : stylesDark}>Flask</p>
             </div>
-            <p style={theme === "light" ? stylesLight : stylesDark} className="aboutSocial">You can contact me through my social media</p>
+            <p
+                style={theme === "light" ? stylesLight : stylesDark}
+                className="aboutSocial">You can contact me through my social media</p>
             <div className="Socials-icon">
-                <a className="linkedInContainer" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/francis-huynh-153246161/">
-                    <i className="linkedinIcon" class="fab fa-linkedin fa-lg" style = {theme === 'light' ? null: stylesDark}></i></a>
-                <a className="githubContainer" target="_blank" rel="noreferrer" href="https://github.com/FrancisHuynh95">
-                    <i className='githubIcon' class="fab fa-github fa-lg" style = {theme === 'light' ? null: stylesDark}></i></a>
+                <a
+                    className="linkedInContainer"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/francis-huynh-153246161/">
+                    <i
+                        className="linkedinIcon"
+                        class="fab fa-linkedin fa-lg"
+                        style={theme === 'light' ? null : stylesDark}></i></a>
+                <a
+                    className="githubContainer"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/FrancisHuynh95">
+                    <i
+                        className='githubIcon'
+                        class="fab fa-github fa-lg"
+                        style={theme === 'light' ? null : stylesDark}></i></a>
             </div>
-            <a style = {theme === 'light' ? null: stylesDark} className="portfolio" target="_blank" rel="noreferrer" href="https://francishuynh95.github.io/">Portfolio</a>
+            <a
+                style={theme === 'light' ? null : stylesDark}
+                className="portfolio"
+                target="_blank"
+                rel="noreferrer"
+                href="https://francishuynh95.github.io/">Portfolio</a>
         </div>
     )
 }
